27ebe4282d5715340ddde48bb1b2500282cd0262
Ashish Yadav <ashish@walkover.in>
2022-12-26 16:36:54 +0530

    init commit

---
+++ assets/js/custom.js
@@ -0,0 +1,18 @@
+function myFunction(x) {
+  x.classList.toggle("change");
+}
+
+
+$(document).ready(function() {
+	$("#testimonial").on("slid.bs.carousel", function () {
+		var image = $(".carousel-item.active").attr('data-image');		
+		$('.testi-box').css('background-image', 'url(' + image + ')');
+		var activeSlide = $(".carousel-indicators .active").index() ;
+		if(activeSlide == 2){
+			$(".slider-heading").text("Freedom + Responsibility at Walkover");
+		}
+		else{
+			$(".slider-heading").text("Voice of walker");
+		}
+	})
+})
