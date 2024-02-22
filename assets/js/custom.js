0870806e8af1ed2e21d922818c09e4cfde1c74f7
Saurabh186 <sawrabhsharma1234@gmail.com>
2024-02-22 12:42:28 +0530

    SAURABH | bug fix | Freedom + Responsibility at Walkover outside carousel inner

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,12 +7,12 @@ $(document).ready(function() {
 	$("#testimonial").on("slid.bs.carousel", function () {
 		var image = $(".carousel-item.active").attr('data-image');		
 		$('.testi-box').css('background-image', 'url(' + image + ')');
-		var activeSlide = $(".carousel-indicators .active").index() ;
-		if(activeSlide == 2){
-			$(".slider-heading").text("Freedom + Responsibility at Walkover");
-		}
-		else{
-			$(".slider-heading").text("Voice of walker");
-		}
+		// var activeSlide = $(".carousel-indicators .active").index() ;
+		// if(activeSlide == 2){
+		// 	$(".slider-heading").text("Freedom + Responsibility at Walkover");
+		// }
+		// else{
+		// 	$(".slider-heading").text("Voice of walker");
+		// }
 	})
 })
